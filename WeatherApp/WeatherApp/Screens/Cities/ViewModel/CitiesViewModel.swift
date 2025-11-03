747e7cd482e1d1b0e0cdadbe0bb44f658f80ccfd
Gegi Ghvachliani <GegiGhvachliani@gmail.com>
2025-11-03 17:00:01 +0400

    add UI

---
+++ WeatherApp/WeatherApp/Screens/Cities/ViewModel/CitiesViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  WeatherApp
+//
+//  Created by Gegi Ghvachliani on 03.11.25.
+//
+//
--- WeatherApp/WeatherApp/Shared/Constants.swift
+++ WeatherApp/WeatherApp/Shared/Constants.swift
@@ -4,4 +4,8 @@
 //
 //  Created by Charles Janjgava on 11/2/25.
 //
-
+import UIKit
+enum ScreenSize {
+    static let width = UIScreen.main.bounds.width
+    static let height = UIScreen.main.bounds.height
+}
