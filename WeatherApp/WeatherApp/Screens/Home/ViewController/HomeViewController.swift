8ec09af63a323bf7b7e44e6920a29113e569209a
ch4r1esJ <Charles.Janjgava.Git@gmail.com>
2025-11-03 15:31:45 +0400

    Add location/top View

--- WeatherApp/WeatherApp/Screens/Home/ViewController/HomeViewController.swift
+++ WeatherApp/WeatherApp/Screens/Home/ViewController/HomeViewController.swift
@@ -11,14 +11,56 @@ class HomeViewController: UIViewController {
     
     // MARK: - Properties
     
+    private let locationView = LocationView()
     
+    private let backgroundImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "defaultBackground")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let titleLabel = UILabel.make(text: "Recommendations", fontSize: 16, weight: .light, color: .black)
+    
+    private let arrowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        let config = UIImage.SymbolConfiguration(pointSize: 20, weight: .light)
+        let image = UIImage(systemName: "arrow.right", withConfiguration: config)
+        button.setImage(image, for: .normal)
+        button.tintColor = .black
+        
+        return button
+    }()
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupBackgroundImage()
+        setupView()
     }
     
     // MARK: - Methods
+    
+    private func setupBackgroundImage() {
+        view.addSubview(backgroundImage)
+        backgroundImage.frame = view.bounds
+    }
+    
+    private func setupView() {
+        view.addSubview(locationView)
+        
+        [locationView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            locationView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            locationView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            locationView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
+        ])
+    }
 }
--- WeatherApp/WeatherApp/Screens/Home/Views/TopView.swift
+++
@@ -1,7 +0,0 @@
-//
-//  TopView.swift
-//  WeatherApp
-//
-//  Created by Charles Janjgava on 11/2/25.
-//
-
